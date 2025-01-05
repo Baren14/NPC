eb29643f9b65546995f5758e6d2fc4d9cd523784
Baren14 <tanjiale2015@gmail.com>
2025-01-05 11:27:36 +0800

    modified:   app/index.html 	modified:   app/index.js

--- app/index.js
+++ app/index.js
@@ -22,7 +22,7 @@ function save(){
 //loading note list
 function load_list(){
     const notes = JSON.parse(localStorage.getItem(storageKey)) || [];
-            const noteList = document.getElementById('saved_notes');
+            const noteList = document.getElementById('list');
             noteList.innerHTML = '';
             notes.forEach(note => {
                 const noteItem = document.createElement('div');
